aa4cec03f89b82a3bc94bfe9786fbdf2f2f38d27
NontasBak <nontasbak@gmail.com>
2024-12-04 11:45:51 +0200

    Remove callList

--- src/main/java/com/cn2/communication/AppController.java
+++ src/main/java/com/cn2/communication/AppController.java
@@ -25,7 +25,6 @@ public class AppController {
     private static final int VOIP_PORT = 9977;
     private static final String IP_ADDRESS = "127.0.0.1";
     private boolean callActive = false;
-    private Map<Integer, Boolean> callUsers = new ConcurrentHashMap<>(); // (userId, isTalking)
     private static List<String> messages = new ArrayList<>();
 
     static {
@@ -86,15 +85,8 @@ public class AppController {
     }
 
     @PostMapping("/call")
-    public int startCall() {
-        if (callUsers.size() >= 2) {
-            return -1;
-        }
+    public void startCall() {
         callActive = true;
-        
-        int userId = callUsers.containsKey(1) ? 2 : 1;
-        
-        callUsers.put(userId, false);
 
         new Thread(() -> {
             try {
@@ -127,10 +119,7 @@ public class AppController {
                     packet = new DatagramPacket(buffer, buffer.length);
                     voipReceiveSocket.receive(packet);
 
-                    boolean isTalking = detectTalking(buffer, bytesRead);
-
-                    // Update the state for the user
-                    callUsers.put(userId, isTalking);
+                    // boolean isTalking = detectTalking(buffer, bytesRead);
 
                     // Play the received audio data
                     sourceLine.write(packet.getData(), 0, packet.getLength());
@@ -139,7 +128,6 @@ public class AppController {
                 ex.printStackTrace();
             }
         }).start();
-        return userId;
     }
 
     private boolean detectTalking(byte[] audioData, int length) {
@@ -158,7 +146,6 @@ public class AppController {
             JsonNode jsonNode = objectMapper.readTree(jsonUserId);
             int userId = jsonNode.get("userId").asInt();
 
-            callUsers.remove(userId);
             callActive = false;
         } catch (IOException e) {
             e.printStackTrace();
@@ -175,12 +162,8 @@ public class AppController {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        
-        return callUsers.getOrDefault(userId, false);
+
+        return false;
     }
 
-    @GetMapping("/callList")
-    public Map<Integer, Boolean> getCallList() {
-        return new ConcurrentHashMap<>(callUsers);
-    }
 }
